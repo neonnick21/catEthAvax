6288b5e0c099df2fe8d4fccbadd42dd5b0b3c0bd
neonnick21 <136787702+neonnick21@users.noreply.github.com>
2023-08-04 15:28:37 +0800

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,5 +4,19 @@
 pragma solidity ^0.8.17;
 
 contract HelloWorld {
-    string public greet = "Hello World!";
+    string public greet = "Hello Everynyan!";
+
+    function requireCat(string arginput) external {
+        require(arginput == "Cat" || arginput == "cat", "That's not a cat /ᐠ_ ꞈ _ᐟ\");
+    }
+
+    function assertCat(uint arginput) external {
+        assert(arginput == "Cat" || arginput == "cat");
+    }
+
+    function revertCat(uint arginput) external {
+        if (arginput == "Cat" || arginput == "cat") {
+            revert("That wasn't a cat! Reverting... /ᐠ_ ꞈ _ᐟ\");
+        }
+    }
 }
